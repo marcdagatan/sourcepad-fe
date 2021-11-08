4e09a0fa0b2b63b1b0bb89e05c13cbdc650f1ea7
Marc Dagatan <marc.dagatan@gmail.com>
2021-11-09 07:58:03 +0800

    Add Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Paper } from '@mui/material';
 
+import { Root } from './store';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Paper>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </Paper>
-    </div>
+    <Root>
+      <div className="App">
+        <Paper>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </Paper>
+      </div>
+    </Root>
   );
 }
 
