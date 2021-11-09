81a96a6de1025f99472825707bb4b540d8099c48
Marc Dagatan <marc.dagatan@gmail.com>
2021-11-09 08:36:11 +0800

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import { Paper } from '@mui/material';
-
 import { Root } from './store';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './routes';
 
-function App() {
-  return (
-    <Root>
-      <div className="App">
-        <Paper>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </Paper>
-      </div>
-    </Root>
-  );
-}
+const App = () => (
+  <Root>
+    <Routes />
+  </Root>
+);
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import '@fontsource/roboto/300.css';
@@ -12,9 +13,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
